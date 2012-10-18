62ec86debcbb5a646acad5228299c3454dfa3880
Mangin.Alexander <mywork.koder@gmail.com>
2012-10-19 00:10:21 +0600

    Refectoring и переход на QUnit
    1. Добавил документацию к публичному методу bubble search.
    2. Переписал тесты c использованием QUnit

---
+++ Task1/js/mysort.js
@@ -0,0 +1,24 @@
+﻿/*global alert: true*/
+/**
+ * Sort array, using Bubble's method
+ *
+ * @param {Array} inputArray - array, which will sort.
+ * @return {Array} The sorted array
+ */
+function bubbleSort(inputArray) {
+    "use strict";
+    if (!((typeof (inputArray) === 'object') && (inputArray instanceof Array))) {
+        throw "You get me not inputArrayay";
+    }
+    var i = 0, j = 0, temp;
+    for (i = 0; i < inputArray.length; i += 1) {
+        for (j = 0; j < inputArray.length; j += 1) {
+            if (inputArray[i] < inputArray[j]) {
+                temp = inputArray[j];
+                inputArray[j] = inputArray[i];
+                inputArray[i] = temp;
+            }
+        }
+    }
+    return inputArray;
+}
